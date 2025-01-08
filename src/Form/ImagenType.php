14669cd772ea9f26fd981178815ae4771447ae67
Antonio Esclapez <titoway33@gmail.com>
2025-01-08 12:43:39 +0100

    4.3 to be finished

--- src/Form/ImagenType.php
+++ src/Form/ImagenType.php
@@ -3,20 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Imagen;
+use PhpParser\Node\Scalar\MagicConst\File;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File as ConstraintsFile;
 
 class ImagenType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre', TextType::class)
+            ->add('nombre', FileType::class, [
+                'label' => 'Nombre imagen (JPG o PNG)',
+                'label_attr' => ['class' => 'etiqueta'],
+                'data_class' => null,
+                'constraints' => [
+                    new ConstraintsFile([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Por favor, seleccione un archivo jpg o png',
+                    ])
+                ],
+            ])
             ->add('descripcion', TextType::class, [
                 'label' => 'Descripción:',
                 'required' => false,
